6318a819409992e94c11e5653844b77829c4760a
LYChubby <f4uzi345@gmail.com>
2025-01-20 23:01:01 +0700

    Membuat Fungsi VillaContainer

---
+++ app/src/main/java/com/example/villaapps/container/VillaContainer.kt
@@ -0,0 +1,30 @@
+package com.example.villaapps.container
+
+import com.example.villaapps.repository.DaftarVillaRepository
+import com.example.villaapps.repository.NetworkDaftarVillaRepository
+import com.example.villaapps.service.VillaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val daftarVillaRepository: DaftarVillaRepository
+}
+
+class VillaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:3000/api/villa/"
+    private val json = Json { ignoreUnknownKeys = true  }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val daftarVillaService: VillaService by lazy {
+        retrofit.create(VillaService::class.java)
+    }
+
+    override val daftarVillaRepository: DaftarVillaRepository by lazy {
+        NetworkDaftarVillaRepository(daftarVillaService)
+    }
+}

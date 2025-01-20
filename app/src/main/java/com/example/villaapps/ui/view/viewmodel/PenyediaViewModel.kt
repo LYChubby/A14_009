45a7d6e93ac03fe6796b27958eb27157cda9eb3a
LYChubby <f4uzi345@gmail.com>
2025-01-21 02:48:35 +0700

    Membuat Fungsi PenyediaViewModel

---
+++ app/src/main/java/com/example/villaapps/ui/view/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.villaapps.ui.view.viewmodel
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.villaapps.VillaApplications
+import com.example.villaapps.ui.view.viewmodel.villaviewmodel.DaftarVillaViewModel
+
+object PenyediaViewModel {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+
+    val Factory = viewModelFactory {
+        initializer {
+            DaftarVillaViewModel(aplikasiReservasi().villacontainer.daftarVillaRepository)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiReservasi(): VillaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as VillaApplications)

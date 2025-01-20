e9be2aea924c0c832dc6c5a0e512d0963215d295
LYChubby <f4uzi345@gmail.com>
2025-01-21 02:20:22 +0700

    Membuat Fungsi Untuk Loading dan Loading Failed

--- app/src/main/java/com/example/villaapps/ui/view/pages/villaview/DaftarVillaView.kt
+++ app/src/main/java/com/example/villaapps/ui/view/pages/villaview/DaftarVillaView.kt
@@ -1,5 +1,6 @@
 package com.example.villaapps.ui.view.pages.villaview
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,10 +10,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -22,6 +25,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.villaapps.model.DaftarVilla
 import com.example.villaapps.navigation.DestinasiNavigasi
@@ -100,3 +105,31 @@ fun DaftarVillaLayout(
         }
     }
 }
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Text(text = "Loading...")
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Loading Failed. Retry?",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = retryAction
+        ) {
+            Text(text = "Retry")
+        }
+    }
+}
+

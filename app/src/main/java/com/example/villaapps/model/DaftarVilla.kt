12f0567d555cafdc20d1972d706761c798b3c507
LYChubby <f4uzi345@gmail.com>
2025-01-20 20:08:22 +0700

    Membuat Data Class Untuk DaftarVilla

---
+++ app/src/main/java/com/example/villaapps/model/DaftarVilla.kt
@@ -0,0 +1,32 @@
+package com.example.villaapps.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DaftarVilla (
+    @SerialName("id_villa")
+    val idVilla: Int,
+
+    @SerialName("nama_villa")
+    val namaVilla: String,
+
+    val alamat: String,
+
+    @SerialName("kamar_tersedia")
+    val kamarTersedia: Int,
+)
+
+@Serializable
+data class DaftarVillaResponse (
+    val status: Boolean,
+    val message: String,
+    val data: List<DaftarVilla>,
+)
+
+@Serializable
+data class DaftarVillaDetailResponse (
+    val status: Boolean,
+    val message: String,
+    val data: DaftarVilla,
+)

4ae7ca37a5a3363f322ccb89cb04fdf1660524c5
LYChubby <f4uzi345@gmail.com>
2025-01-21 19:18:21 +0700

    Menambah Fungsi Route Ke Halaman Detail

--- app/src/main/java/com/example/villaapps/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/villaapps/navigation/PengelolaHalaman.kt
@@ -5,14 +5,18 @@ import androidx.annotation.RequiresExtension
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.villaapps.ui.view.pages.DestinasiHome
 import com.example.villaapps.ui.view.pages.HomeView
 import com.example.villaapps.ui.view.pages.villaview.DaftarVillaScreen
+import com.example.villaapps.ui.view.pages.villaview.DestinasiDetailVilla
 import com.example.villaapps.ui.view.pages.villaview.DestinasiInsertVilla
 import com.example.villaapps.ui.view.pages.villaview.DestinasiVilla
+import com.example.villaapps.ui.view.pages.villaview.DetailVillaView
 import com.example.villaapps.ui.view.pages.villaview.EntryVillaScreen
 
 @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
@@ -47,6 +51,31 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+
+        composable(
+            DestinasiDetailVilla.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetailVilla.IDVILLA) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            val idVilla = it.arguments?.getInt(DestinasiDetailVilla.IDVILLA)
+            idVilla?.let { idVilla ->
+                DetailVillaView(
+                    navigateBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+//                        navController.navigate("${DestinasiUpdate.route}/$it")
+                    },
+                    idVilla = idVilla,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
         }
     }
 }

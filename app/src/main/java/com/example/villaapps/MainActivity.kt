fe9cc8cc5b91877360f625b9eca263e5a258175c
LYChubby <f4uzi345@gmail.com>
2025-01-21 23:28:09 +0700

    Membuat Fungsi UpdatePelangganViewModel

--- app/src/main/java/com/example/villaapps/MainActivity.kt
+++ app/src/main/java/com/example/villaapps/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.villaapps
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,17 +13,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.villaapps.navigation.PengelolaHalaman
 import com.example.villaapps.ui.theme.VillaAppsTheme
+import com.example.villaapps.ui.view.viewmodel.ReservasiVillaApp
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             VillaAppsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ReservasiVillaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }

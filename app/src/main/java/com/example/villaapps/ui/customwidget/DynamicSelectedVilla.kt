5e6d8fb912d05923fad1f837f5639527d6072884
LYChubby <f4uzi345@gmail.com>
2025-01-22 22:59:02 +0700

    Membuat Fungsi ExposedDropdownMenuBox Untuk Nama Villa dan Pelanggan

---
+++ app/src/main/java/com/example/villaapps/ui/customwidget/DynamicSelectedVilla.kt
@@ -0,0 +1,61 @@
+package com.example.villaapps.ui.customwidget
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedVilla(
+    modifier: Modifier = Modifier,
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            options.forEach { villa ->
+                DropdownMenuItem(
+                    text = { Text(text = villa) },
+                    onClick = {
+                        onValueChangedEvent(villa)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}

912fb0bcdb9d94c8c9baa80ace3109ca6fa62e82
LYChubby <f4uzi345@gmail.com>
2025-01-25 01:16:55 +0700

    Membuat Fungsi DetailReviewView

---
+++ app/src/main/java/com/example/villaapps/ui/view/pages/reviewview/DetailReviewView.kt
@@ -0,0 +1,245 @@
+package com.example.villaapps.ui.view.pages.reviewview
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.villaapps.model.Pelanggan
+import com.example.villaapps.model.Review
+import com.example.villaapps.navigation.DestinasiNavigasi
+import com.example.villaapps.ui.customwidget.CostumeTopAppBar
+import com.example.villaapps.ui.view.viewmodel.PenyediaViewModel
+import com.example.villaapps.ui.view.viewmodel.reviewviewmodel.DetailReviewUiState
+import com.example.villaapps.ui.view.viewmodel.reviewviewmodel.DetailReviewViewModel
+
+object DestinasiDetailReview: DestinasiNavigasi {
+    override val route = "detail_Review"
+    override val titleRes = "Detail Review"
+    const val IDREVIEW = "idReview"
+    val routeWithArgs = "$route/{$IDREVIEW}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
+
+
+@Composable
+fun ComponentDetailReview(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun ItemDetailReview(
+    modifier: Modifier = Modifier,
+    review: Review,
+    pelanggan: Pelanggan
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailReview(judul = "Nama", isinya = pelanggan.namaPelanggan)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailReview(judul = "Nilai", isinya = review.nilai)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailReview(judul = "Komentar", isinya = review.komentar)
+            Spacer(modifier = Modifier.padding(4.dp))
+        }
+    }
+}
+
+@Composable
+fun BodyDetailReview(
+    modifier: Modifier = Modifier,
+    detailReviewUiState: DetailReviewUiState,
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+    when (detailReviewUiState) {
+        is DetailReviewUiState.Loading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        is DetailReviewUiState.Success -> {
+            Column(
+                modifier = modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                ItemDetailReview(
+                    review = detailReviewUiState.review,
+                    pelanggan = detailReviewUiState.pelanggan,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = { deleteConfirmationRequired = true },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        is DetailReviewUiState.Error -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak Ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailReviewView(
+    idReview: Int,
+    modifier: Modifier = Modifier,
+    viewModel: DetailReviewViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    navigateBack: () -> Unit,
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+
+    LaunchedEffect(idReview) {
+        viewModel.getReviewById()
+    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetailReview.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onEditClick(idReview.toString()) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+
+        BodyDetailReview(
+            modifier = modifier.padding(innerPadding),
+            detailReviewUiState = viewModel.detailReviewUiState,
+            onDeleteClick = {
+                viewModel.deleteReview(idReview)
+                onDeleteClick()
+            }
+        )
+    }
+}

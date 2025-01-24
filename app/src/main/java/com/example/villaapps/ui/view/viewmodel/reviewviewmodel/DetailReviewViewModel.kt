2a67f2a946f5681e11dbb8724846ce0d4b250be6
LYChubby <f4uzi345@gmail.com>
2025-01-24 23:13:04 +0700

    Membuat Fungsi DetailReviewViewModel

---
+++ app/src/main/java/com/example/villaapps/ui/view/viewmodel/reviewviewmodel/DetailReviewViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.villaapps.ui.view.viewmodel.reviewviewmodel
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.villaapps.model.Review
+import com.example.villaapps.repository.ReviewRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class DetailReviewUiState {
+    data class Success(val review: Review) : DetailReviewUiState()
+    object Error : DetailReviewUiState()
+    object Loading : DetailReviewUiState()
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+class DetailReviewViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val reviewRepository: ReviewRepository
+): ViewModel() {
+    private val idReview: Int = checkNotNull(savedStateHandle[DestinasiDetailReview.IDREVIEW])
+
+    var detailReviewUiState: DetailReviewUiState by mutableStateOf(DetailReviewUiState.Loading)
+        private set
+
+    init {
+        getReviewById()
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getReviewById() {
+        viewModelScope.launch {
+            detailReviewUiState = DetailReviewUiState.Loading
+            detailReviewUiState = try {
+                val review = reviewRepository.getReviewById(idReview)
+                DetailReviewUiState.Success(review)
+            } catch (e: IOException) {
+                DetailReviewUiState.Error
+            } catch (e: HttpException) {
+                DetailReviewUiState.Error
+            }
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun deleteReview() {
+        viewModelScope.launch {
+            try {
+                reviewRepository.deleteReview(idReview)
+            } catch (e: IOException) {
+                DetailReviewUiState.Error
+            } catch (e: HttpException) {
+                DetailReviewUiState.Error
+            }
+        }
+    }
+}
